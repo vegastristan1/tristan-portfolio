2c2a66a39b7eb259278717d50a46c412b552ec9e
Tristan Vegas <vegastristan1@gmail.com>
2024-09-22 04:50:12 +0800

    test route and view connection

---
+++ resources/views/new.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tristan Vegas</title>
+</head>
+
+<body>
+    <h1>Hello, Tristan!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, natus animi! Officiis maxime hic, mollitia
+        optio sint rerum deserunt laboriosam cum? Dolores, harum placeat. Magni dolor esse ab quo numquam!</p>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -24,4 +24,9 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+
+Route::get('/new', function () {
+    return view('new');
+});
