5b54daa9bcb4b00d02d4d8fce37b2950ca27cf6d
Tristan Vegas <vegastristan1@gmail.com>
2024-09-22 22:49:10 +0800

    sample preserve-scroll page

--- resources/js/Pages/PracticeWelcome.vue
+++ resources/js/Pages/PracticeWelcome.vue
@@ -12,4 +12,7 @@
     <h1>{{ $page.component }}</h1> <!-- this can also use to get the file name and as the title page  -->
 
     <h1>{{ $page.props.practice_user }}</h1>
+
+    <Link class="mt-[600px] block" href="/practice" preserve-scroll>Refresh</Link>
+    <!-- the preserve-scroll helps the user to pause view position if submitting form that push back to the top of the page -->
 </template>
