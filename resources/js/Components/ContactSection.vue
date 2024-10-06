23f181e9d9302858082e6fec20b39749e4c2f9a7
Tristan Vegas <vegastristan1@gmail.com>
2024-10-06 12:58:18 +0800

    update contact section open link

--- resources/js/Components/ContactSection.vue
+++ resources/js/Components/ContactSection.vue
@@ -1,22 +1,23 @@
 <template>
     <ul class="space-y-2 font-medium place-self-center border-gray-200 text-sm sm:text-sm md:text-sm lg:text-sm">
         <li>
-            <a href="#"
+            <a href="mailto:vegastristan1@gmail.com"
                 class="flex items-center p-2 text-gray-400 transition duration-75 rounded-lg hover:bg-gray-700 hover:text-white group">
                 <font-awesome-icon icon="fa-solid fa-at" />
                 <span class="ms-3">vegastristan1@gmail.com</span>
             </a>
         </li>
         <li>
-            <a href="#"
+            <a href="tel:+639212089062"
                 class="flex items-center p-2 text-gray-400 transition duration-75 rounded-lg hover:bg-gray-700 hover:text-white group">
                 <font-awesome-icon icon="fa-solid fa-phone" />
                 <span class="ms-3">+63-921-208-9062</span>
             </a>
         </li>
         <li>
-            <a href="#"
-                class="flex items-center p-2 text-gray-400 transition duration-75 rounded-lg hover:bg-gray-700 hover:text-white group">
+            <a href="https://www.google.com/maps/search/?api=1&query=San+Fernando,+Camarines+Sur,+Philippines"
+                class="flex items-center p-2 text-gray-400 transition duration-75 rounded-lg hover:bg-gray-700 hover:text-white group"
+                target="_blank">
                 <font-awesome-icon icon="fa-solid fa-location-pin" />
                 <span class="ms-3">San Fernando, Camarines Sur, Philippines</span>
             </a>
