f7a1f8641e0423301da1b86c7dcf0b13f7e161f2
Tristan Vegas <vegastristan1@gmail.com>
2024-10-30 22:37:01 +0800

    fixed meta description

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,9 +10,9 @@
     <!-- Primary Meta Tags -->
     <meta name="title" content="Tristan Vegas" />
     <meta name="description"
-        content="Information Technology graduate with extensive experience developing websites, managing IT support more broadly as well associated infrastructure Committed to human-centered web solutions and teamwork." />
+        content="Information Technology graduate with extensive experience developing websites, managing IT support more broadly as well associated infrastructure Committed to human-centered web solutions and teamwork" />
     <meta property="description"
-        content="Information Technology graduate with extensive experience developing websites, managing IT support more broadly as well associated infrastructure Committed to human-centered web solutions and teamwork." />
+        content="Information Technology graduate with extensive experience developing websites, managing IT support more broadly as well associated infrastructure Committed to human-centered web solutions and teamwork" />
 
     <!-- Open Graph / Facebook -->
     <meta property="og:site_name" content="https://tristan-portfolio-ecru.vercel.app/">
@@ -20,7 +20,7 @@
     <meta property="og:url" content="https://tristan-portfolio-ecru.vercel.app/" />
     <meta property="og:title" content="Tristan Vegas" />
     <meta property="og:description"
-        content="Information Technology graduate with extensive experience developing websites, managing IT support more broadly as well associated infrastructure Committed to human-centered web solutions and teamwork." />
+        content="Information Technology graduate with extensive experience developing websites, managing IT support more broadly as well associated infrastructure Committed to human-centered web solutions and teamwork" />
     {{-- <meta property="og:image" content="{{ asset('images/1730290810451rp3j7ax7.jpg') }}" /> --}}
     <meta property="og:image" content="{{ asset('hero-image-1.png') }}" />
 
@@ -29,8 +29,7 @@
     <meta property="twitter:url" content="https://tristan-portfolio-ecru.vercel.app/" />
     <meta property="twitter:title" content="Tristan Vegas" />
     <meta property="twitter:description"
-        content="Information Technology graduate with extensive experience developing websites, managing IT support more broadly as well associated infrastructure Committed to human-centered web solutions and teamwork." />
-    {{-- <meta property="twitter:image" content="{{ asset('images/1730290810451rp3j7ax7.jpg') }}" /> --}}
+        content="Information Technology graduate with extensive experience developing websites, managing IT support more broadly as well associated infrastructure Committed to human-centered web solutions and teamwork" />
     <meta property="twitter:image" content="{{ asset('images/1730290810451rp3j7ax7.jpg') }}" />
 
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon/apple-touch-icon.png') }}">
