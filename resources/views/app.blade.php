2c5488e672aeee6d5e086eea0f19ffa890d99044
Tristan Vegas <vegastristan1@gmail.com>
2024-10-30 22:56:39 +0800

    metadata description test

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -17,18 +17,21 @@
     <meta property="og:type" content="website" />
     <meta property="og:url" content="https://tristan-portfolio-ecru.vercel.app/" />
     <meta property="og:title" content="Tristan Vegas" />
-    <meta property="og:image" content="{{ asset('images/1730290810451rp3j7ax7.jpg') }}" />
+    <meta property="og:image" content="{{ asset('images/hero-image-1.png') }}" />
 
     <!-- Twitter -->
     <meta property="twitter:card" content="summary_large_image" />
     <meta property="twitter:url" content="https://tristan-portfolio-ecru.vercel.app/" />
     <meta property="twitter:title" content="Tristan Vegas" />
     <meta property="twitter:description"
-        content="Information Technology graduate with extensive experience developing websites, managing IT support more broadly as well associated infrastructure Committed to human-centered web solutions and teamwork">
+        content="Graduate of BS in IT with expertise in web development, technical support, and CMS setup, delivering user-centered web solutions for clients. Collaborative and growth-focused." />
+
     <meta property="description"
-        content="Information Technology graduate with extensive experience developing websites, managing IT support more broadly as well associated infrastructure Committed to human-centered web solutions and teamwork" />
+        content="IT graduate and skilled web developer, specializing in dynamic, user-focused websites, technical support, and CMS solutions that empower client independence." />
+
     <meta property="og:description"
-        content="Information Technology graduate with extensive experience developing websites, managing IT support more broadly as well associated infrastructure Committed to human-centered web solutions and teamwork" />
+        content="Experienced IT professional in web development, specializing in dynamic and user-centered websites. Passionate about creating seamless solutions and thriving in collaborative environments." />
+
     <meta property="twitter:image" content="{{ asset('images/1730290810451rp3j7ax7.jpg') }}" />
 
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon/apple-touch-icon.png') }}">
